dd20750d03b58aefd79e0c1d5f3f73fa5dcae957
GiorgioSV99 <118373082+GiorgioSV99@users.noreply.github.com>
2023-12-29 12:28:57 +0100

    Add files via upload

--- ProjectCV.py
+++ ProjectCV.py
@@ -25,11 +25,15 @@ car_image = pygame.transform.flip(car_image, True, False) #trasla la barca per v
 car_image_x = 0
 car_image_y = (screen_height - car_height) // 2
 # Carica l'immagine di sfondo
-background_image = pygame.image.load("watertex.jpg")
-background_width, background_height = background_image.get_size()
+background1 = pygame.image.load("watertex.jpg")
+background2 = pygame.transform.flip(background1, True, False)
+background_width, background_height = background1.get_size()
 overlap = 0  # Lunghezza della zona di sovrapposizione
 # Posizioni iniziali delle due copie dell'immagine di sfondo
-background_x1, background_x2 = 0, background_width
+
+
+background1_x, background2_x = 0, background_width
+
 # Carica le immagini degli ostacoli con le relative maschere di collisione
 obstacle_images = [pygame.image.load("barrel.png").convert_alpha(),
     # Aggiungi altre immagini degli ostacoli con le relative maschere...
@@ -56,14 +60,30 @@ bullet_speed = 20  # Velocità di spostamento delle palline
 last_shot_time = time.time()
 shoot_cooldown = 0.75  # Tempo in secondi tra uno sparo e l'altro
 obstacle_spawn_prob = 0.01  # Probabilità iniziale di spawn
-obstacle_spawn_interval = 5  # Intervallo di tempo per aumentare la probabilità (in secondi)
+obstacle_spawn_interval = 3  # Intervallo di tempo per aumentare la probabilità (in secondi)
 obstacle_last_spawn_time = time.time()
 punteggio_monete = 0
 coin_spawn_prob = 0.005  # Probabilità iniziale di spawn
-coin_spawn_interval = 5  # Intervallo di tempo per aumentare la probabilità (in secondi)
+coin_spawn_interval = 4  # Intervallo di tempo per aumentare la probabilità (in secondi)
 coin_last_spawn_time = time.time()
 start_time = pygame.time.get_ticks()  # Ottieni il tempo di inizio in millisecondi
 distanza_percorsa = 0
+
+vite = 3
+cuori = []
+cuore_image = pygame.image.load("cuore.png").convert_alpha()
+cuore_width, cuore_height = 50, 50  # Regola le dimensioni del cuore a tuo piacimento
+cuore_image = pygame.transform.scale(cuore_image, (cuore_width, cuore_height))
+cuore_spawn_prob = 0.001  # Probabilità iniziale di spawn
+cuore_spawn_interval = 5  # Intervallo di tempo per aumentare la probabilità (in secondi)
+cuore_last_spawn_time = time.time()
+
+max_obstacles = 15  # Modifica il numero massimo di ostacoli desiderato
+max_coins = 5      # Modifica il numero massimo di monete desiderato
+max_cuori = 1      # Modifica il numero massimo di cuori desiderato
+
+
+
 
 def check_collision(rect1, rect2):
     return rect1.colliderect(rect2)  #vede se le due immagini si sovrappongono (collidono)
@@ -77,14 +97,17 @@ def show_game_over_text():
 
 # Funzione per reimpostare la posizione della nave e degli asset e tutti gli altri parametri
 def reset_game():
-    global car_image_x, car_image_y, obstacles, coins, bullets, obstacle_spawn_prob, coin_spawn_prob, distanza_percorsa, punteggio_monete
+    global car_image_x, car_image_y, obstacles, coins, cuori, bullets, obstacle_spawn_prob, coin_spawn_prob, distanza_percorsa, punteggio_monete, vite,cuore_spawn_prob
     car_image_x = 0
     car_image_y = (screen_height - car_height) // 2
     obstacles = []
     coins = []
     bullets = []
+    cuori=[]
+    vite=3
     obstacle_spawn_prob = 0.01  # Puoi reimpostare la probabilità iniziale di spawn se necessario
     coin_spawn_prob = 0.005
+    cuore_spawn_prob = 0.001
     distanza_percorsa = 0
     punteggio_monete = 0
 # Impostazioni della mano, TODO: provare a fare fine-tuning sugli ultimi due parametri
@@ -96,14 +119,11 @@ hands = mp_hands.Hands(
     min_tracking_confidence=0.02 #threshold di tracking del mani in movimento
 )
 #avvia telecamera SE NE HAI UNA ESTERNA USA QUELLA ALTRIMENTI QUELLA PREDEFINITA
-try:
-    cap = cv2.VideoCapture(1)
-except:
-    cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 # Aggiorna la direzione di sterzata
-steering_threshold_lower = -5
-steering_threshold_upper = 5
+steering_threshold_lower = -10
+steering_threshold_upper = 10
 
 
 
@@ -111,7 +131,7 @@ steering_threshold_upper = 5
 
 
 while cap.isOpened():
-    clock.tick(60)  # Imposta il numero di fotogrammi al secondo desiderato
+    clock.tick(30)  # Imposta il numero di fotogrammi al secondo desiderato
     ret, frame = cap.read()
     if not ret:
         break
@@ -249,27 +269,25 @@ while cap.isOpened():
     #screen.fill((255, 255, 255))  # Sfondo bianco
     
     
-    speedbg=0
-    # Sposta l'immagine di sfondo
-    background_x1 -= speedbg
-    background_x2 -= speedbg
+    speed=10
+    # Sposta le immagini in orizzontale
+    background1_x -= speed
+    background2_x -= speed
 
-    # Ricomincia l'immagine di sfondo dal lato opposto quando raggiunge la fine
-    if background_x1 <= -background_width + overlap:
-        background_x1 = background_width - overlap
+    # Se un'immagine raggiunge la fine della finestra, riportala alla posizione iniziale
+    if background1_x <= -background_width:
+        background1_x = background2_x + background_width
 
-    if background_x2 <= -background_width + overlap:
-        background_x2 = background_width - overlap
+    if background2_x <= -background_width:
+        background2_x = background1_x + background_width
 
-    # Disegna l'immagine di sfondo
-    screen.blit(car_image, (car_image_x, car_image_y))
-    pygame.display.flip()
 
-    screen.blit(background_image, (background_x1, 0))
-    screen.blit(background_image, (background_x2, 0))
+    # Disegna le immagini nello sfondo
+    screen.blit(background1, (background1_x, 0))
+    screen.blit(background2, (background2_x, 0))
 
-    
-   
+
+
 
     # Aggiorna la lista degli ostacoli
     for obstacle in obstacles:
@@ -277,6 +295,7 @@ while cap.isOpened():
     # Rimuovi gli ostacoli che sono usciti dallo schermo
     obstacles = [obstacle for obstacle in obstacles if obstacle['x'] + obstacle_width > 0]
     # Aggiorna la lista degli ostacoli
+    
     for coin in coins:
         coin['x'] -= coin_speed
     # Rimuovi gli ostacoli che sono usciti dallo schermo
@@ -284,8 +303,18 @@ while cap.isOpened():
     # Aggiungi nuovi ostacoli casuali con probabilità basata sul tempo trascorso
     current_time = time.time()
     time_since_last_spawn = current_time - obstacle_last_spawn_time
+    time_since_last_spawn_coin = current_time - coin_last_spawn_time
+    time_since_last_spawn_cuore = current_time - cuore_last_spawn_time
+    # Aggiorna la lista degli ostacoli
+    cuore_speed = 10
+    for cuore in cuori:
+        cuore['x'] -= cuore_speed
+    # Rimuovi gli ostacoli che sono usciti dallo schermo
+    cuori = [cuore for cuore in cuori if cuore['x'] + cuore_width > 0]
 
-    if random.random() < obstacle_spawn_prob:
+
+    #obstacle_last_spawn_time = current_time - obstacle_last_spawn_time
+    if len(obstacles)<max_obstacles and random.random() < obstacle_spawn_prob:
         new_obstacle = {
             'image': random.choice(obstacle_images),
             'x': screen_width,
@@ -294,9 +323,9 @@ while cap.isOpened():
         obstacles.append(new_obstacle)
     # Aumenta progressivamente la probabilità di spawn
     if time_since_last_spawn > obstacle_spawn_interval:
-        obstacle_spawn_prob *= 1.2  # Puoi modificare il fattore di aumento come preferisci
+        obstacle_spawn_prob *= 1.1  # Puoi modificare il fattore di aumento come preferisci
         obstacle_last_spawn_time = current_time
-    if random.random() < coin_spawn_prob:
+    if len(coins)<max_coins and random.random() < coin_spawn_prob:
         new_coin = {
             'image': coin_image,
             'x': screen_width,
@@ -304,16 +333,33 @@ while cap.isOpened():
         }
         coins.append(new_coin)
     # Aumenta progressivamente la probabilità di spawn
-    time_since_last_spawn_coin = current_time - obstacle_last_spawn_time
+    #coin_last_spawn_time = current_time - coin_last_spawn_time
     if time_since_last_spawn_coin > coin_spawn_interval:
-        coin_spawn_prob *= 1.2  # Puoi modificare il fattore di aumento come preferisci
+        coin_spawn_prob *= 1.1  # Puoi modificare il fattore di aumento come preferisci
         coin_last_spawn_time = current_time
+        
+    if len(cuori)<max_cuori and random.random() < cuore_spawn_prob:
+        new_cuore = {
+            'image': cuore_image,
+            'x': screen_width,
+            'y': random.randint(0, screen_height - cuore_height),
+        }
+        cuori.append(new_cuore)
+    # Aumenta progressivamente la probabilità di spawn
+    #cuore_last_spawn_time = current_time - cuore_last_spawn_time
+    if time_since_last_spawn_cuore > cuore_spawn_interval:
+        cuore_spawn_prob *= 1.1  # Puoi modificare il fattore di aumento come preferisci
+        cuore_last_spawn_time = current_time
+        
+        
     # Disegna gli ostacoli
     for obstacle in obstacles:
         screen.blit(obstacle['image'], (obstacle['x'], obstacle['y']))
     # Disegna gli ostacoli
     for coin in coins:
         screen.blit(coin['image'], (coin['x'], coin['y']))
+    for cuore in cuori:
+        screen.blit(cuore['image'], (cuore['x'], cuore['y']))
     # Disegna l'asset del gioco sulla schermata
     screen.blit(car_image, (car_image_x, car_image_y))
     # Verifica collisione con ostacoli
@@ -321,14 +367,26 @@ while cap.isOpened():
     for obstacle in obstacles:
         obstacle_rect = pygame.Rect(obstacle['x'], obstacle['y'], obstacle_width, obstacle_height)
         if check_collision(car_rect, obstacle_rect):
-            show_game_over_text()
-            # Nel ciclo principale del gioco
+            vite -= 1
+            #Rimuovi l'ostacolo colpito
+            obstacles.remove(obstacle)
+            if vite <= 0:
+                show_game_over_text()
+
+
     for coin in coins:
         coin_rect = pygame.Rect(coin['x'], coin['y'], coin_width, coin_height)
         if check_collision(car_rect, coin_rect):
             # La nave ha colpito una moneta, incrementa il punteggio
             punteggio_monete += 1
             coins.remove(coin)  # Rimuovi la moneta dalla lista
+    for cuore in cuori:
+       cuore_rect = pygame.Rect(cuore['x'], cuore['y'], cuore_width, cuore_height)
+       if check_collision(car_rect, cuore_rect):
+           # La nave ha preso un cuore, incrementa le vite
+           if(vite<3):
+               vite += 1
+           cuori.remove(cuore)  # Rimuovi il cuore dalla lista
     for bullet in bullets:
         bullet['x'] += bullet_speed
         bullet_radius = 18
@@ -341,7 +399,8 @@ while cap.isOpened():
                 # Rimuovi l'ostacolo colpito
                 obstacles.remove(obstacle)
                 # Rimuovi la pallina
-                bullets.remove(bullet)
+                if(len(bullets)>0):
+                    bullets.remove(bullet)
                 break  # Esci dal ciclo degli ostacoli dopo la prima collisione
         for coin in coins:
             coin_rect = pygame.Rect(coin['x'], coin['y'], coin_width, coin_height)
@@ -349,7 +408,19 @@ while cap.isOpened():
                 # Rimuovi l'ostacolo colpito
                 coins.remove(coin)
                 # Rimuovi la pallina
-                bullets.remove(bullet)
+                if(len(bullets)>0):
+                    bullets.remove(bullet)
+                break  # Esci dal ciclo degli ostacoli dopo la prima collisione
+        for cuore in cuori:
+            cuore_rect = pygame.Rect(cuore['x'], cuore['y'], cuore_width, cuore_height)
+            if check_collision(bullet_rect, cuore_rect):
+                if(vite<3):
+                    vite += 1
+                # Rimuovi l'ostacolo colpito
+                cuori.remove(cuore)
+                # Rimuovi la pallina
+                if(len(bullets)>0):
+                    bullets.remove(bullet)
                 break  # Esci dal ciclo degli ostacoli dopo la prima collisione
     # Dopo la parte del rilevamento della mano
     current_time = time.time()
@@ -363,14 +434,16 @@ while cap.isOpened():
     elapsed_time = (current_time - start_time) / 1000  # Tempo trascorso in secondi
 
     # Ogni 5 secondi, aggiorna la distanza percorsa
-    if elapsed_time >= 2:
+    if elapsed_time >= 1:
         distanza_percorsa += 1
         start_time = current_time  # Resetta il tempo di inizio per il prossimo intervallo di 5 secondi
     font = pygame.font.Font(None, 36)  # Scegli una dimensione del carattere
-    punteggio_text = font.render(f"Punteggio: {punteggio_monete}", True, (255, 255, 255))
+    punteggio_text = font.render(f"Monete: {punteggio_monete}", True, (255, 255, 255))
     distanza_text = font.render(f"Distanza: {distanza_percorsa} metri", True, (255, 255, 255))
+    vite_text = font.render(f"Vite: {vite}", True, (255, 255, 255))
     screen.blit(punteggio_text, (10, 10))
     screen.blit(distanza_text, (10, 50))
+    screen.blit(vite_text, (10, 100))
     pygame.display.flip()    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -385,5 +458,3 @@ while cap.isOpened():
 
 cap.release()
 cv2.destroyAllWindows()
-
-
